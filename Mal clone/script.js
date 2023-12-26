f9db16361b3903ce1d1ec68f6349941d7b12b321
Banyu_sans <banyuarfa1122@gmail.com>
2023-12-26 03:58:31 -0800

    mal

--- Mal clone/script.js
+++ Mal clone/script.js
@@ -2,25 +2,24 @@ async function getTopAnime() {
   const topAnime = await fetch("https://api.jikan.moe/v4/top/anime").then(
     (response) => response.json()
   );
-  const animes = topAnime.data;
+  const animes = topAnime.data.filter(anime => anime.status === 'Currently Airing');
   topAnimeUI(animes);
 }
 
 function topAnimeUI(animes) {
   let cards = "";
 
-  for (let i = 0; i < 5; i++) {
+  for (let i = 0; i < animes.length; i++) {
     const anime = animes[i];
-    cards += showTopAnime(anime);
+    cards += showTopAnime(anime, i);
   }
   const animeContainer = document.querySelector(".top-anime-list");
   animeContainer.innerHTML = cards;
 }
 
-function showTopAnime(anime) {
-  console.log(anime);
+function showTopAnime(anime, i) {
   return `<div class='top-anime'>
-            <p class='rank'>${anime.rank}</p>
+            <p class='rank'>${i + 1}</p>
             <a href=''><img src="${anime.images.jpg.image_url}"></a>
             <div class='top-anime-description'>
               <a href=''>${anime.title}</a>

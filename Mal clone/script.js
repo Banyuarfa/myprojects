3ee1343c806c5e884d4a38d60c1c6c1d285a0966
Banyu_sans <banyuarfa1122@gmail.com>
2023-12-29 02:48:56 -0800

    mal

--- Mal clone/script.js
+++ Mal clone/script.js
@@ -1,33 +1,30 @@
-async function getTopAnime() {
-  const topAnime = await fetch("https://api.jikan.moe/v4/top/anime").then(
+const api = "https://api.jikan.moe/v4";
+
+const getAnime = async () => {
+  const topFetch = await fetch(`${api}/top/anime?filter=airing&&sfw=true`).then(
     (response) => response.json()
   );
-  const animes = topAnime.data.filter(anime => anime.status === 'Currently Airing');
-  topAnimeUI(animes);
-}
-
-function topAnimeUI(animes) {
-  let cards = "";
-  for (let i = 0; i < animes.length; i++) {
-    const anime = animes[i];
-    cards += showTopAnime(anime, i);
-  }
-  const animeContainer = document.querySelector(".top-anime-list");
-  animeContainer.innerHTML = cards;
-}
-
-function showTopAnime(anime, i) {
-  return `<div class='top-anime'>
-            <p class='rank'>${i + 1}</p>
-            <a href=''><img src="${anime.images.jpg.image_url}"></a>
-            <div class='top-anime-description'>
-              <a href=''>${anime.title}</a>
-              <div>
-                <p>${anime.type}, ${anime.episodes} eps, scored ${anime.score}</p>
-                <p>${anime.members} members</p>
+  const topDatas = topFetch.data;
+  console.log(topDatas);
+  showAnime(topDatas);
+};
+const showAnime = (topDatas) => {
+  let card = "";
+  topDatas.forEach((data) => {
+    return (card += animeUI(data));
+  });
+  const cardContainer = document.querySelector("#top-anime-container");
+  cardContainer.innerHTML = card;
+};
+const animeUI = (data) => {
+  return `<div id='anime-UI'>
+            <a href='${data.url}'>
+              <img src='${data.images.jpg.image_url}'>
+              <div class='title'>
+                <h4>${data.title}</h4>
               </div>
-            </div>
-            <button>add</button>
+            </a>
+            <p>score ${data.score}</p>
           </div>`;
-}
-getTopAnime();
+};
+getAnime();
